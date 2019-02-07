914c1da78464a256bf9b298277e94c266c3b8d27
Brian Martinez <brianmartinez@brians-mac.local>
2019-02-07 12:46:08 -0800

    Gallery set up (Swift and Storyboard)

---
+++ Flixr/MovieGalleryCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGalleryCell.swift
+//  Flixr
+//
+//  Created by Brian Martinez on 2/7/19.
+//  Copyright © 2019 Brian Martinez. All rights reserved.
+//
+
+import UIKit
+
+class MovieGalleryCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
